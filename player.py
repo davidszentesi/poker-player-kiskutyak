4942a9dff6dd7b3f71edb0967741e90de5a85170
Gábor Kovács <gabor950601@gmail.com>
2018-10-01 10:32:10 +0200

    raise default

--- player.py
+++ player.py
@@ -1,9 +1,9 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "0.1"
 
     def betRequest(self, game_state):
-        return 100
+        return game_state['current_buy_in'] - game_state['players']['in_action']['bet'] + game_state['minimum_raise']
 
     def showdown(self, game_state):
         pass

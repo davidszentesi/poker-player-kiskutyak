00b8df99573a3232cf82a0663773d8ed860d1cf2
David Szentesi <szntsd@gmail.com>
2018-10-01 11:32:31 +0200

    pair in hand

--- player.py
+++ player.py
@@ -1,9 +1,14 @@
 
 class Player:
-    VERSION = "0.1"
+    VERSION = "0.3"
 
     def betRequest(self, game_state):
-        return 100
+        our_player = game_state["players"]["in_action"]
+        our_cards = our_player["hole_cards"]
+        if our_cards[0]["rank"] == our_cards[1]["rank"] and our_cards[0]["rank"] >= 7:
+            return game_state["current_buy_in"] - our_player["bet"] + game_state["minimum_raise"]
+        else:
+            return 100
 
     def showdown(self, game_state):
         pass

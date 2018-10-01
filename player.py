33b93c1238ffea744d71a6fb0d56fe9fa4123fc5
Tamás Varjas <tamas.varjas00@gmail.com>
2018-10-01 11:37:42 +0200

    add logging

--- player.py
+++ player.py
@@ -1,11 +1,16 @@
+from __future__ import print_function
+
+import sys
+
 
 class Player:
-    VERSION = "0.3"
+    VERSION = "0.3.1"
 
     def betRequest(self, game_state):
         our_player = game_state["players"]["in_action"]
         our_cards = our_player["hole_cards"]
         if our_cards[0]["rank"] == our_cards[1]["rank"] and our_cards[0]["rank"] >= 7:
+            Player.log(self, "Hand in pair")
             return game_state["current_buy_in"] - our_player["bet"] + game_state["minimum_raise"]
         else:
             return 200
@@ -13,3 +18,5 @@ class Player:
     def showdown(self, game_state):
         pass
 
+    def log(self, message):
+        print(message, file=sys.stderr)

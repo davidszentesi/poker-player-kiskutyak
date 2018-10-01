8c640333a5b2d1a5e0367043465041190ffe1f09
Gábor Kovács <gabor950601@gmail.com>
2018-10-01 11:23:24 +0200

    200

--- player.py
+++ player.py
@@ -1,10 +1,9 @@
 
 class Player:
-    VERSION = "0.1"
+    VERSION = "0.2"
 
     def betRequest(self, game_state):
-        return 100
-
+        return 200
     def showdown(self, game_state):
         pass
 

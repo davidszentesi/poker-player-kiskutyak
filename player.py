da8aabecbd18cf91a37da3b7892cb6f91aa140f0
Tamás Varjas <tamas.varjas00@gmail.com>
2018-10-01 11:43:05 +0200

    update logging message

--- player.py
+++ player.py
@@ -4,13 +4,13 @@ import sys
 
 
 class Player:
-    VERSION = "0.3.1"
+    VERSION = "0.3.2"
 
     def betRequest(self, game_state):
         our_player = game_state["players"]["in_action"]
         our_cards = our_player["hole_cards"]
         if our_cards[0]["rank"] == our_cards[1]["rank"] and our_cards[0]["rank"] >= 7:
-            Player.log(self, "Pair in hand")
+            Player.log(self, "Pair in hand bet")
             return game_state["current_buy_in"] - our_player["bet"] + game_state["minimum_raise"]
         else:
             return 200

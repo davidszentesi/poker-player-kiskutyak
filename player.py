b3523f29416b2208230b0cb8db4e5bffd88df824
David Szentesi <szntsd@gmail.com>
2018-10-01 11:49:33 +0200

    pair in hand enhancement, big combo in hand

--- player.py
+++ player.py
@@ -1,12 +1,26 @@
 
 class Player:
-    VERSION = "0.3"
+    VERSION = "0.4"
 
     def betRequest(self, game_state):
         our_player = game_state["players"]["in_action"]
         our_cards = our_player["hole_cards"]
-        if our_cards[0]["rank"] == our_cards[1]["rank"] and our_cards[0]["rank"] >= 7:
+        first_card = our_cards[0]
+        second_card = our_cards[1]
+        figures = ["J", "Q", "K", "A"]
+        call = game_state["current_buy_in"] - our_player["in_action"]["bet"]
+
+        # hands in pair
+        if first_card["rank"] == second_card["rank"] and (first_card["rank"] >= 7 or first_card["rank"] in figures):
             return game_state["current_buy_in"] - our_player["bet"] + game_state["minimum_raise"]
+
+        elif (first_card["rank"] >= 7 and second_card["rank"] in figures) or (second_card["rank"] >= 7 and first_card["rank"] in figures):
+
+            if call < our_player["stack"] * 0.1:
+                return call
+            else:
+                return 0
+
         else:
             return 200
 
